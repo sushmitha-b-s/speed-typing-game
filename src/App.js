a8443be8901b1423b529d2f31b44cb21b1523a58
Sushmitha BS <bssushmitha26@gmail.com>
2020-08-21 16:36:30 +0200

    added basic styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import './App.css'
+import './styles/index.scss'
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="container">
+      <main className="game">
+        <h1 className="game__title">How fast can you type?</h1>
+
+        <div className="game__content">
+          <textarea
+            name="text"
+            placeholder="start typing here..."
+            className="game__content__text"
+          />
+
+          <h3 className="game__timeRemaining">Time remaining: 10s</h3>
+
+          <button className="game__startBtn">start game</button>
+
+          <h3 className="game__wordCount">word count: 0</h3>
+        </div>
+      </main>
     </div>
   )
 }
